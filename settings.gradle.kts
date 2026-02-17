46a5490b7d8b83384e4bba3e70ce328b5c6f6d95
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-02-17 01:40:25 +0100

    2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
-// Settings
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VoiceDeutschMaster"
+include(":app")
