a2da471bdbe4f04c474b94a8eaf9837ff475da5d
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-02-17 01:42:17 +0100

    3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,10 @@
-// Root build
-
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.room) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+}
