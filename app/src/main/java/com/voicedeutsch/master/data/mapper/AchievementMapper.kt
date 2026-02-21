f9e18664ce3e3dbc174e01c23100bdf0cad9e554
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-02-21 12:13:37 +0100

    рнне

---
+++ app/src/main/java/com/voicedeutsch/master/data/mapper/AchievementMapper.kt
@@ -0,0 +1,8 @@
+package com.voicedeutsch.master.data.mapper
+
+/**
+ * Achievement mapping is handled inline inside AchievementRepositoryImpl
+ * because the Achievement system uses sealed-class conditions that require
+ * Json access for serialization. This file is a placeholder for future
+ * extraction if the mapping logic grows.
+ */
