f8cab2d5a26f8bd3979aea3ae28952ec7561309c
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-02-22 02:18:27 +0100

    usywyw

--- app/src/main/java/com/voicedeutsch/master/domain/usecase/achievement/GetUserAchievementsUseCase.kt
+++ app/src/main/java/com/voicedeutsch/master/domain/usecase/achievement/GetUserAchievementsUseCase.kt
@@ -10,23 +10,3 @@ class GetUserAchievementsUseCase(
         return achievementRepository.getUserAchievements(userId)
     }
 }
-
-
-// FILE: app/src/main/java/com/voicedeutsch/master/domain/usecase/achievement/GetUnannouncedAchievementsUseCase.kt
-
-package com.voicedeutsch.master.domain.usecase.achievement
-
-import com.voicedeutsch.master.domain.model.achievement.UserAchievement
-import com.voicedeutsch.master.domain.repository.AchievementRepository
-
-/**
- * Returns achievements that Voice hasn't yet announced to the user.
- * Called by FunctionRouter for get_unannounced_achievements().
- */
-class GetUnannouncedAchievementsUseCase(
-    private val achievementRepository: AchievementRepository
-) {
-    suspend operator fun invoke(userId: String): List<UserAchievement> {
-        return achievementRepository.getUnannouncedAchievements(userId)
-    }
-}
