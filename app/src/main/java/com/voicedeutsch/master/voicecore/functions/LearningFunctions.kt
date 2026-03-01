4f55733affc5eba88c4fecb61eeb027e481ae152
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-01 13:10:13 +0100

    ушшугу

--- app/src/main/java/com/voicedeutsch/master/voicecore/functions/LearningFunctions.kt
+++ app/src/main/java/com/voicedeutsch/master/voicecore/functions/LearningFunctions.kt
@@ -24,6 +24,8 @@ object LearningFunctions {
         FunctionRegistry.declare(
             name = "get_pronunciation_targets",
             description = "Получить звуки и слова для тренировки произношения.",
+            // ✅ ИСПРАВЛЕНИЕ: Добавлен dummy-параметр
+            params = mapOf("dummy" to ("string" to "Игнорируемый параметр, передай 'ok'")),
         ),
     )
 }
