c885b1c3226c1bffcb632629e01905117b096806
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-03 13:10:15 +0100

    кк

--- app/src/main/java/com/voicedeutsch/master/presentation/screen/knowledge/KnowledgeScreen.kt
+++ app/src/main/java/com/voicedeutsch/master/presentation/screen/knowledge/KnowledgeScreen.kt
@@ -40,7 +40,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -66,7 +66,7 @@ fun KnowledgeScreen(
     onStartSession: () -> Unit = {},
     viewModel: KnowledgeViewModel = koinViewModel(),
 ) {
-    val state by viewModel.uiState.collectAsState()
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
 
     Scaffold(
         containerColor = Background,
