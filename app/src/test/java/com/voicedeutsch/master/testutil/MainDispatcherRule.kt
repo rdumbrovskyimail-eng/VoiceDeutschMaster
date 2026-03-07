e7de3990f2ac11aca2ed92b5970f26271a75c785
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-07 12:12:53 +0100

    yeye

---
+++ app/src/test/java/com/voicedeutsch/master/testutil/MainDispatcherRule.kt
@@ -0,0 +1,17 @@
+package com.voicedeutsch.master.testutil
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainDispatcherRule : BeforeEachCallback, AfterEachCallback {
+    val testDispatcher = StandardTestDispatcher()
+    override fun beforeEach(context: ExtensionContext?) { Dispatchers.setMain(testDispatcher) }
+    override fun afterEach(context: ExtensionContext?) { Dispatchers.resetMain() }
+}
