288855b59ff6c1891791470b14925bc2b396f6bd
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-03-15 03:41:51 +0100

    Update file

--- app/src/main/java/com/voicedeutsch/master/app/di/AppModule.kt
+++ app/src/main/java/com/voicedeutsch/master/app/di/AppModule.kt
@@ -2,12 +2,14 @@ package com.voicedeutsch.master.app.di
 
 import com.voicedeutsch.master.data.local.JsonFactory
 import com.voicedeutsch.master.util.NetworkMonitor
+import com.voicedeutsch.master.voicecore.engine.AvatarAudioAnalyzer
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
     single { JsonFactory.instance }
     single { NetworkMonitor(androidContext()) }
+    single { AvatarAudioAnalyzer() }
 }
 
 val appModules = listOf(
